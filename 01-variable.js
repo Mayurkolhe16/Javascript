1f7a589af542a989b20bdcd2eb34a51f4af4ffb9
mayurkolhe16 <ramchandrakolhe6@gmail.com>
2024-06-06 14:17:12 +0530

    practice

---
+++ 01-variable.js
@@ -0,0 +1,19 @@
+const accountId = 13456
+let accountEmail = "mayurkolhe@gmail.com"
+var accountpass = "Mayur1234"
+accountcity = "Rahuri"
+
+//const accountId = 84960 // not allowed
+
+accountEmail = "ram@gmail.com"
+accountpass = "12345"
+accountcity = "pune"
+
+console.log(accountId);
+
+/*
+
+
+*/
+
+console.table([accountEmail , accountId , accountpass, accountcity])
